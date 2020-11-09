d2abc97cdeb43bfa63fac8de7f4878c0523b26f0
yangzq80 <yangzq80@gmail.com>
2020-11-09 09:53:15 +0800

    bug(webssh.js):modify endpoint

--- src/main/resources/static/js/webssh.js
+++ src/main/resources/static/js/webssh.js
@@ -7,7 +7,7 @@ WSSHClient.prototype._generateEndpoint = function () {
     } else {
         var protocol = 'ws://';
     }
-    var endpoint = protocol+window.location.host+':'+window.location.port+'/webssh';
+    var endpoint = protocol+window.location.host+'/webssh';
     return endpoint;
 };
 
